b913fdda9a3e1ecb9d423b482577c75eb92a80f1
FirelightFlagboy <firelight.flagboy@gmail.com>
2018-03-04 11:08:00 +0100

    work on snake game

--- snake/js/sketch.js
+++ snake/js/sketch.js
@@ -1,61 +1,79 @@
 let s;
 let scl = 10;
+let life = 1;
 let rows;
 let cols;
 let food;
 
 function setup() {
-    createCanvas(600, 600);
+	createCanvas(600, 600);
 
-    rows = height / scl;
-    cols = width / scl;
+	rows = height / scl;
+	cols = width / scl;
 
-    s = new Snake(cols / 2, rows / 2, 1, 0);
-    frameRate(10);
-    pickLocation();
+	s = new Snake(cols / 2, rows / 2, 1, 0);
+	frameRate(10);
+	pickLocation();
 }
 
 function pickLocation() {
-    let x = floor(random(0, cols));
-    let y = floor(random(0, rows));
-    food = createVector(x, y);
+	let x = floor(random(0, cols));
+	let y = floor(random(0, rows));
+	food = createVector(x * scl, y * scl);
 }
 
 function keyPressed() {
-    switch (keyCode) {
-        case UP_ARROW:
-            console.log("up");
-            s.dir(0, -1);
-            break;
-        case DOWN_ARROW:
-            console.log("down");
-            s.dir(0, 1);
-            break;
-        case LEFT_ARROW:
-            console.log("left");
-            s.dir(-1, 0);
-            break;
-        case RIGHT_ARROW:
-            console.log("right");
-            s.dir(1, 0);
-            break;
-    }
+	switch (keyCode) {
+		case UP_ARROW:
+			s.dir(0, -1);
+			break;
+		case DOWN_ARROW:
+			s.dir(0, 1);
+			break;
+		case LEFT_ARROW:
+			s.dir(-1, 0);
+			break;
+		case RIGHT_ARROW:
+			s.dir(1, 0);
+			break;
+		case ENTER:
+			if (life == 0) {
+				life = 1;
+				draw();
+				s = new Snake(cols / 2, rows / 2, 1, 0);
+				frameRate(10);
+			}
+			break;
+	}
 
+}
+
+function mousePressed() {
+	s.grow();
 }
 
 function checkFood() {
-    if (s.x == food.x && s.y == food.y)
-        return (true);
-    return (false);
+	if (s.x == food.x && s.y == food.y)
+		return (true);
+	return (false);
 }
 
 function draw() {
-    background(0);
-    s.update();
-    s.show();
-    if (checkFood() == true) {
-        pickLocation();
-    }
-    fill(255, 0, 150);
-    rect(food.x * scl, food.y * scl, scl, scl);
+	background(0);
+	s.update();
+	if (s.death()) {
+		fill(0, 150, 250);
+		textSize(32);
+		textAlign(CENTER, CENTER);
+		life = 0;
+		text("You lose,\npress enter to continue", width / 2 - 17, height / 2);
+		frameRate(0);
+	}
+	s.show();
+	if (checkFood() == true) {
+		s.grow();
+		pickLocation();
+	}
+	fill(255, 0, 150);
+	rect(food.x, food.y, scl, scl);
 }
--- snake/js/snake.js
+++ snake/js/snake.js
@@ -1,26 +1,71 @@
+function constrainSnake(n, low, high) {
+	if (n < low) {
+		return (high);
+	} else if (n > high) {
+		return (low);
+	}
+	return (n);
+}
+
 function Snake(x, y, xSpeed, ySpeed) {
-    this.x = x;
-    this.y = y;
-    this.len = 0
-    this.s = [];
-    this.xSpeed = xSpeed;
-    this.ySpeed = ySpeed;
+	this.x = x;
+	this.y = y;
+	this.len = 1;
+	this.s = [createVector(x, y)];
+	this.xSpeed = xSpeed;
+	this.ySpeed = ySpeed;
 
-    this.dir = function(xSpeed, ySpeed) {
-        this.xSpeed = xSpeed;
-        this.ySpeed = ySpeed;
-    }
+	this.dir = function(xSpeed, ySpeed) {
+		if (this.s.length > 1) {
+			if (this.ySpeed == -1 && ySpeed == 1) //0,-1	up
+				return;
+			else if (this.ySpeed == 1 && ySpeed == -1) //0,1	down
+				return;
+			else if (this.xSpeed == 1 && xSpeed == -1) //-1,0	left
+				return;
+			else if (this.xSpeed == -1 && xSpeed == 1) //1,0	right
+				return;
+		}
+		this.ySpeed = ySpeed;
+		this.xSpeed = xSpeed;
+	}
 
-    this.update = function() {
-        this.x += this.xSpeed * scl;
-        this.y += this.ySpeed * scl;
+	this.grow = function() {
+		this.len++;
+		console.log("grow: " + this.len);
+	}
 
-        this.x = constrain(this.x, 0, width - scl);
-        this.y = constrain(this.y, 0, height - scl);
-    }
+	this.death = function() {
+		if (this.s.length > 1) {
+			for (var i = 0; i < this.s.length - 1; i++) {
+				if (this.s[i].x == this.x && this.s[i].y == this.y) {
+					console.log(i + ": :" + this.s[i]);
+					return (true);
+				}
+			}
+		}
+		return (false);
+	}
+	this.update = function() {
+		if (this.len == this.s.length) {
+			for (let i = 0; i < this.s.length - 1; i++) {
+				this.s[i] = this.s[i + 1];
+			}
+		}
+		this.s[this.len - 1] = createVector(this.x, this.y);
+		this.x += this.xSpeed * scl;
+		this.y += this.ySpeed * scl;
 
-    this.show = function() {
-        fill(255);
-        rect(this.x, this.y, scl, scl);
-    }
+		this.x = constrainSnake(this.x, 0, width - scl);
+		this.y = constrainSnake(this.y, 0, height - scl);
+
+	}
+
+	this.show = function() {
+		for (var i = 0; i < this.s.length; i++) {
+			fill(255 - i * 4, 92 + i * 4, 255);
+			let vec = this.s[i];
+			rect(vec.x, vec.y, scl, scl);
+		}
+	}
 }
